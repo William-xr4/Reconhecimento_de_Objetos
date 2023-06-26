cdbc4f2e14f977fe928c41a6d3b7596bfc6cc598
William-xr4 <108703320+William-xr4@users.noreply.github.com>
2023-06-26 11:47:43 -0300

    Add files via upload
    Hospedando meu site!

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+  });
+  
+  var camera=document.getElementById("camera");
+  Webcam.attach("#camera");
+  
+  function Tirar_Foto(){
+    Webcam.snap(
+      function(dataURI){
+        document.getElementById("result").innerHTML='<img id="foto" src="'+dataURI+'">';
+      }
+    );
+  }
+  console.log("versão ml5:", ml5.version);
+  
+  var classifier = ml5.imageClassifier(https:/teachablemachine.withgoogle.com/models/vD3USY8oG/1,modelLoaded);
+  function modelLoaded(){
+    console.log("Modelo carregado!")
+  }
+  
+  function Checar(){
+    var img= document.getElementById("foto");
+    classifier.classify(img, Pegar_resultado);
+  }
+  
+  function Pegar_resultado(error, results){
+    if(error){
+      console.error(error);
+    }
+    else{
+      console.log(results);
+      document.getElementById("resultObjectName").innerHTML=results[0].label;
+      document.getElementById("resultObjectAccuracy").innerHTML=(results[0].confidence.toFixed(3))*100+"%";
+    }
+  }
